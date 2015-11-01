09a4291121940e98aab0f5627fb0db56de7471a6
Michael <fluidmotionstudios@outlook.com>
2015-11-01 11:36:05 -0500

    Added cocoon for nested forms and added ingredient model

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ gem 'turbolinks'
 gem 'haml-rails', '~> 0.9.0'
 gem 'simple_form', '~> 3.2'
 gem "paperclip", "~> 4.3"
+gem 'cocoon', '~> 1.2', '>= 1.2.6'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,6 @@
 class Recipe < ActiveRecord::Base
   has_attached_file :image, styles: { medium: "300x300#" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  has_many :ingredients
+  accepts_nested_attributes_for :ingredients, reject_if: :all_blank, allow_destroy: true
 end
